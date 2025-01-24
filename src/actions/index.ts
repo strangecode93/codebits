16f9a4bf55ee0956800f090fa74de8851d6a3c86
strange <strangecode93@gmail.com>
2025-01-24 19:31:18 +0530

    error handling

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use server"
 
 import { prisma } from "@/lib/prisma"
@@ -22,5 +23,32 @@ export const deleteSnippet = async (id:number) => {
             id: id
         }
     })
+    redirect("/")
+}
+
+export async function createSnippet(prevState: {message: string}, formData: FormData) {
+
+    // 'use server'
+    try {
+        const title = formData.get('title')
+        const code = formData.get('code')
+        if(typeof title !== "string" || title.length < 4){ 
+            return {message: "Title must be at least 4 characters"}
+        }
+        if(typeof code !== "string" || code.length < 4){ 
+            return {message: "Code must be at least 4 characters"}
+        }
+        const snippet = await prisma.snippets.create({
+            data: {
+                title: title,
+                code: code
+            }
+        })
+        console.log(snippet)
+        throw new Error("oops! something went wrong")
+    } catch (error : any) {
+        return {message: error.message}
+    }
+
     redirect("/")
 }
--- src/app/snippet/new/page.tsx
+++ src/app/snippet/new/page.tsx
@@ -1,30 +1,20 @@
+"use client"
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Textarea } from '@/components/ui/textarea'
-import { prisma } from '@/lib/prisma'
-import { ArrowLeft } from 'lucide-react'
+import { ArrowLeft, CircleAlert } from 'lucide-react'
 import Link from 'next/link'
-import { redirect } from 'next/navigation'
-import React from 'react'
+// import { prisma } from '@/lib/prisma'
+// import { redirect } from 'next/navigation'
+import React, { useActionState } from 'react'
+import * as actions from "@/actions"
 
 const CreateSnippetPage = () => {
-    async function createSnippet(formData: FormData) {
-        'use server'
-        const title = formData.get('title')
-        const code = formData.get('code')
-        const snippet = await prisma.snippets.create({
-            data: {
-                title: title as string,
-                code: code as string
-            }
-        })
-        console.log(snippet)
-
-        redirect("/")
-    }
+    const [formStateData, xyz] = useActionState(actions.createSnippet, {message:""})
+    
   return (
-    <form action={createSnippet}>
+    <form action={xyz}>
         <div className='flex items-center justify-between'>
         <h1 className='text-3xl font-semibold pb-10'>create-bits</h1>
         <Link href='/'>
@@ -39,6 +29,9 @@ const CreateSnippetPage = () => {
             <Label>Code</Label>
             <Textarea name='code' id='code'/>
         </div>
+        {
+            formStateData.message && <p className='text-red-500 mb-2 border border-red-500 rounded p-2 opacity-70'><span className='flex items-center gap-2'><CircleAlert size={20}/>{formStateData.message}</span></p>
+        }
         <Button className='font-bold'>New</Button>
     </form>
   )
